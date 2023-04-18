9c9478c6d7d05667cc2f242848f4643438c34cf7
Soubhik Maji <67087252+majisoubhik01@users.noreply.github.com>
2023-04-18 20:30:29 +0530

    Update StreamlitDep.py

--- StreamlitDep.py
+++ StreamlitDep.py
@@ -249,6 +249,7 @@ if choice == 'Nifty50':
                 col1, mid, col2 = st.columns([15,5,50])
                 with col1:
                     #st.pyplot(fig)
+                    st.text("Yay! You are in the right place!")
                 with col2:
                     st.table(portfolios[Results_oos['sharp_ratio_oos'].idxmax()]) 
                 
@@ -447,7 +448,7 @@ elif choice == 'Inter-sector':
                 ax1.pie(portfolios[Results_oos['sharp_ratio_oos'].idxmax()], labels= portfolios.index, autopct='%.2f', textprops={'fontsize': 20});
                 ax1.set_title('Portfolio Allocations',fontsize = 30)
                 st.pyplot(fig)
-            st.table(portfolios[Results_oos['sharp_ratio_oos'].idxmax()])
+                st.table(portfolios[Results_oos['sharp_ratio_oos'].idxmax()])
 else:
     sector = st.selectbox("Select sector:",["AUTO","BANKING","FINANCIAL",
                                             "FMCG","HEALTHCARE","IT",
@@ -709,6 +710,6 @@ else:
                 ax1.pie(portfolios[Results_oos['sharp_ratio_oos'].idxmax()], labels= portfolios.index, autopct='%.2f', textprops={'fontsize': 20});
                 ax1.set_title('Portfolio Allocations',fontsize = 30)
                 st.pyplot(fig)
-            st.table(portfolios[Results_oos['sharp_ratio_oos'].idxmax()])
+                st.table(portfolios[Results_oos['sharp_ratio_oos'].idxmax()])
 
 
