155ed8d74ad7ba644bd6983c2453f35c7b808e5d
Soubhik Maji <67087252+majisoubhik01@users.noreply.github.com>
2023-04-25 22:10:27 +0530

    Update StreamlitDep.py

--- StreamlitDep.py
+++ StreamlitDep.py
@@ -29,7 +29,9 @@ from cvxopt import blas, solvers
 
 algo = "HRP"
 st.set_page_config(layout="wide")
-st.image("StockEasy Logo.png", width = 300)
+left, mid, right = st.columns([40,20,40])
+with mid:
+    st.image("StockEasy Logo.png", width = 300)
 # col1, mid, col2 = st.columns([15,5,50])
 # with col1:
 #     st.image('Logo_header@2x.jpg',width=300)
